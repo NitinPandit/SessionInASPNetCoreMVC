672cafe25a4c62eee50ec25280e46fe4ea793cfd
Nitin Pandit <donly@SLS-NSHARMA21.infragistics.local>
2022-08-29 13:28:17 +0530

    Final Code

---
+++ WebApplicationTestingSesstion/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationTestingSesstion.Models;
+using Newtonsoft.Json;
+
+namespace WebApplicationTestingSesstion.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddNew(Employee employee)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            if(!string.IsNullOrEmpty(HttpContext.Session.GetString("employeesList")))
+            {
+                var session = HttpContext.Session.GetString("employeesList");
+
+                employees=JsonConvert.DeserializeObject<List<Employee>>(session);
+            }
+
+            employees.Add(employee);
+
+            HttpContext.Session.SetString("employeesList", JsonConvert.SerializeObject(employees));
+
+            return Json(employees);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplicationTestingSesstion/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationTestingSesstion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
